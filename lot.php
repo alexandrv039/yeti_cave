fd1c16e8a473af8c50fe3d50f64e67e4fddf7cca
alexandrv039 <alexandrv039@gmail.com>
2022-10-26 07:32:20 +0500

    lot page

--- lot.php
+++ lot.php
@@ -11,8 +11,9 @@ require_once 'models.php';
 require_once 'helpers.php';
 require_once 'functions.php';
 
-if (isset($_GET['id'])) {
-    $lot = getLot($con, $_GET['id']);
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+if ($id) {
+    $lot = getLot($con, $id);
     $categories = getCategories($con);
     $page_content = include_template('lot_template.php', ['lot' => $lot]);
     $layout_content = include_template('layout.php', ['is_auth' => $is_auth, 'user_name' => $user_name, 'content' => $page_content,
@@ -20,7 +21,8 @@ if (isset($_GET['id'])) {
 
     print ($layout_content);
 } else {
-    print 'Error';
+    http_response_code(404);
+    die();
 }
 
 
